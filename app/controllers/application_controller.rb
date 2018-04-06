04eafff61a36f42c631dded8399bee5a071debd2
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-06 05:53:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    redirect to "/posts"
+  end
+  
+  get "/posts" do
+    @posts = Post.all
+    erb :index
+  end
+  
+  get "/posts/new" do
+    erb :new
+  end
+  
+  post "/posts" do 
+    Post.create(params)
+    redirect to "/posts"
+  end
+  
+  get "/posts/:id" do 
+    @post = Post.find(params[:id])
+    erb :show
+  end
+  
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+  
+  patch "/posts/:id" do 
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+  
+  delete "/posts/:id/delete" do 
+    Post.find(params[:id]).destroy
+    erb :delete
   end
 end
